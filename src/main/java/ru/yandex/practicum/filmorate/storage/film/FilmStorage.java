8c141dffe01cc5543ffd5ac24b7dff7e2c00b255
Zigvert <dark133788@gmail.com>
2025-03-10 17:53:22 +0300

    Attempt #2

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 public interface FilmStorage {
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(Long id);
+
     Film getFilmById(Long id);
+
     List<Film> getAllFilms();
+    
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -4,9 +4,15 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(Long id);
+
     User getUserById(Long id);
+
     List<User> getAllUsers();
+
 }
